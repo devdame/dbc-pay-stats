ca6a8a09fb89a84983cfa76953157355074b146d
Celeen <celeenrusk@gmail.com>
2015-12-18 12:18:35 -0600

    adds config options for CAS SSO

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :ensure_loggedin
+
+  def ensure_loggedin
+    if session['cas'].nil? || session['cas']['user'].nil?
+      render status: 401, text: "redirecting to SSO..."
+    end
+  end
 end
