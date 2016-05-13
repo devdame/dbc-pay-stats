d0167936b49e2fc07bc162e1d9b538329f7d782e
Scott <code@getbraintree.com>
2016-05-13 17:01:09 -0500

    Added some rinky-dink seeds for messing around in console during development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+location1 = Location.create(city: "Chicago", region: "Illinois", country: "USA", geoname_id: "10101")
+location2 = Location.create(city: "Mars City", region: "Mars", country: "Space", geoname_id: "666")
+
+
+student = Student.create(gender: "f", dbc_location: "chicago")
+
+job1 = Job.create(student: student, start_date: 2.years.ago, end_date: 1.years.ago, work_type: "product", title: "astronaut")
+salary1 = Salary.create(job: job1, start_date: 2.years.ago, end_date: 1.years.ago, negotiated: false, notes: "it was kool or whatever", location: location1)
+
+job2 = Job.create(student: student, start_date: 1.years.ago, title: "chief of police")
+salary2 = Salary.create(job: job2, start_date: 1.years.ago, end_date: 5.weeks.ago, negotiated: true, notes: "whoah, space", location: location2)
+salary3 = Salary.create(job: job2, start_date: 5.weeks.ago, negotiated: false, notes: "came back home", location: location1)
